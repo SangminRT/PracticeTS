3a41ea14fbefd3950e3eead6ad6c6439d6a29f02
sangminRT <sangmin613@gmail.com>
2020-05-12 13:07:02 +0900

    e.g. import

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,7 +1,15 @@
 import {testMakePerson} from "./utils/makePerson";
-import {IPerson, makePerson} from "./utils/Person";
+import IPerson, {makePerson} from "./person/Person";
+import Chance from "chance";
+import * as R from 'ramda';
+import Person from "./person/Person";
 
-testMakePerson()
+testMakePerson();
+
+const chance = new Chance();
+let persons: IPerson[] = R.range(0, 2)
+    .map((n: number) => new Person(chance.name(), chance.age()));
+console.log(persons);
 
 const testMakePerson_2 = (): void => {
     let jane: IPerson = makePerson('Jane');
