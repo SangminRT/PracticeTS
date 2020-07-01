977101481a995c4c4d9d1b603e041c73412bcacd
sanhan <sangmin613@gmail.com>
2020-07-01 13:14:46 +0900

    20.07.01

---
+++ ch09-1/src/addIndex.ts
@@ -0,0 +1,8 @@
+import * as R from 'ramda';
+
+const addIndex = R.pipe(
+    R.addIndex(R.map)(R.add),
+    // R.addIndex(R.map)(value: number, index: number) =>R.add(value)(index)),
+    R.tap(a => console.log(a))
+)
+const newNumbers = addIndex(R.range(1, 9 + 1));
